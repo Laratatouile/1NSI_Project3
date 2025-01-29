62321d1f3916e8478a074b48e94899559bfb0c3e
Laratatouile <maxpublic09@gmail.com>
2025-01-29 17:34:33 +0100

    29/01

--- chiffre_dechiffre.py
+++ chiffre_dechiffre.py
@@ -1,16 +1,12 @@
 def code(texte:str, decalage:int) -> str:
     """ chiffre le texte avec le decalage """
-    dictionnaire_decalage = cree_dictionnaire_decalage(decalage)
-    chaine_fin = "".join([dictionnaire_decalage.get(i) for i in texte])
-    return chaine_fin
+    return "".join([cree_dictionnaire_decalage(decalage).get(i) for i in texte])
 
 
 
 def decode(texte:str, decalage:int) -> str:
     """ dechiffre le texte avec le decalage """
-    dictionnaire_decalage = cree_dictionnaire_decalage(-decalage)
-    chaine_fin = "".join([dictionnaire_decalage.get(i) for i in texte])
-    return chaine_fin
+    return "".join([cree_dictionnaire_decalage(-decalage).get(i) for i in texte])
 
 
 def dechiffrage_maj(texte:str, decalage:int) -> str:
@@ -18,13 +14,11 @@ def dechiffrage_maj(texte:str, decalage:int) -> str:
     dechiffre le texte avec le decalage
     (!) Uniquement avec le majuscules sans espaces
     """
-    dictionnaire_decalage = {chr(i+65):chr((i+decalage) %26+65) for i in range(26)}
-    return "".join([dictionnaire_decalage.get(i) if i != None else "" for i in texte.upper()])
+    return "".join([{chr(i+65):chr((i+decalage) %26+65) for i in range(26)}.get(i) if i != None else "" for i in texte.upper()])
 
 
 
 def cree_dictionnaire_decalage(decalage:int) -> dict:
     """ crée un dictionnaire avec un decalage de decalage """
     symboles_dans_dictionnaire = [chr(i) for i in range(65, 91)] + [chr(i) for i in range(97, 123)] + [chr(i) for i in range(48, 58)] + [" "]
-    dictionnaire = {i[1] : symboles_dans_dictionnaire[i[0]+decalage if i[0]+decalage < len(symboles_dans_dictionnaire) else i[0] - len(symboles_dans_dictionnaire) + decalage] for i in enumerate(symboles_dans_dictionnaire)}
-    return dictionnaire
+    return {i[1] : symboles_dans_dictionnaire[i[0]+decalage if i[0]+decalage < len(symboles_dans_dictionnaire) else i[0] - len(symboles_dans_dictionnaire) + decalage] for i in enumerate(symboles_dans_dictionnaire)}
--- dechiffrage.py
+++ dechiffrage.py
@@ -7,10 +7,9 @@ def dechiffrage(texte:str) -> tuple:
     dict_apparitions = dictionnaire(texte)
     # la lettre qui revient le plus souvent
     lettre_plus = le_plus(dict_apparitions)
-    # le dictionnaire qui contient chaque lettre dans l'alphabet et son rang dans celui-ci
-    dictionnaire_lettre_chiffre = {chr(i): i - 64 for i in range(65, 91)}
-    # calcul du decalage a appliquer pour retrouver le texte d'origine
-    decalage = 5 - dictionnaire_lettre_chiffre[lettre_plus]
+    # calcul du decalage a appliquer pour retrouver le texte d'origine à partir d'un dictionnaire
+    # qui contient chaque lettre dans l'alphabet avec son rang
+    decalage = 5 - {chr(i): i - 64 for i in range(65, 91)}[lettre_plus]
     # dechffrage du texte a l'aide du module de chiffrage_dechiffrage
     texte_dechiffre = ch_dech.dechiffrage_maj(texte, decalage)
     return texte_dechiffre, -decalage
@@ -20,19 +19,9 @@ def dechiffrage(texte:str) -> tuple:
 
 def le_plus(dictionnaire:dict) -> str:
     """ calcul de la lettre qui revient le plus souvent """
-    lettre_max = ["", 0]
-    for i in dictionnaire:
-        if dictionnaire[i] > lettre_max[1]:
-            lettre_max = [i, dictionnaire[i]]
-    return lettre_max[0]
+    return max(dictionnaire, key=dictionnaire.get)
 
 
 def dictionnaire(texte:str) -> dict:
     """ compte le nombre d'apparition des lettres dans le texte """
-    dictionnaire = {}
-    for i in texte:
-        if i in dictionnaire:
-            dictionnaire[i] += 1
-        else:
-            dictionnaire[i] = 1
-    return dictionnaire
+    return {char: texte.count(char) for char in texte}
